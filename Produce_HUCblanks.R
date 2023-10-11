f8a807e151d6c9eac928f7eb0d64b764b8b4a4ae
Caelum Mroczek <112019115+cmroczek97@users.noreply.github.com>
2023-10-10 21:54:13 -0700

    working working

--- Produce_HUCblanks.R
+++ Produce_HUCblanks.R
@@ -13,21 +13,22 @@ invisible(lapply(packages, library, character.only = TRUE))
 
 ##Load in data
 #dataset with stream reach code and lat/long for desired river
-River_Points <- read_csv("/Users/Caelum/Desktop/SaltRiver_points.csv") 
+file_base <- "/Users/caelum/Library/Mobile Documents/com~apple~CloudDocs/NAU/Research/AZBFI_Manuscript/"
+
+River_Points <- read_csv(paste0(file_base, "SaltRiver/SaltRiver_points.csv"))
 
 #dataset of all predictors, keyed to HUC8 number
-HUC_Predictors <- read_csv("/Users/Caelum/Library/Mobile Documents/com~apple~CloudDocs/NAU/Research/AZBFI_Manuscript/VariableData/HUC_Variables/HUC_Dataset.csv")
+HUC_Predictors <- read_csv(paste0(file_base, "/VariableData/HUC_Variables/HUC_Dataset.csv"))
 
-HUC8_Shape <- read_sf(dsn = "/Volumes/Mroczek,Caelum", layer = "HUC8_AZ")
-####################################################################################
-HUC8_Shape_sp <- as(HUC8_Shape, "Spatial")
+r <- rast(x = "/Volumes/Mroczek,Caelum/Data/HUC8_raster.tif")
+r <- project(r, "+proj=longlat")
 
-# Now try using over function with SpatialPoints and Spatial object
-point <- st_transform(point, crs = st_crs(HUC8_Shape))
-over(point, HUC8_Shape_sp)
+p <- vect(River_Points[5,], geom=c("Long","Lat"))
 
-point <- SpatialPoints(River_Points[1,])
-sp::over(point,HUC8_Shape)
+e <- extract(r,p)
+
+
+
 
 
 site_calcs <- function(dataset){
